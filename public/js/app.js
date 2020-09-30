d72246445a338b36c330d2a8d613b49d4a844fe6
sudo-APT-GIT <paishreya1412@gmail.com>
2020-10-01 01:16:42 +0530

    Added some dynamic styles

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,27 @@
-const weatherForm = document.querySelector('form')
-const search = document.querySelector('input')
-const messageOne = document.querySelector('#msg-1')
-const messageTwo = document.querySelector('#msg-2')
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#msg-1");
+const messageTwo = document.querySelector("#msg-2");
 
-weatherForm.addEventListener('submit', (e) => {
-    e.preventDefault()
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const location = search.value
+  const location = search.value;
 
-    messageOne.textContent = 'Loading ...'
-    messageTwo.textContent = ''
+  messageOne.textContent = "Loading ...";
+  messageTwo.textContent = "";
 
-    fetch('/weather?address=' + location).then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                messageOne.textContent = data.error
-            } else {
-                messageOne.textContent = 'Location: ' + data.location
-                messageTwo.textContent =  data.forecast
-            }
-        })
-    })
-})
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+        messageOne.style.color = "red";
+      } else {
+        messageOne.style.color = "blue";
+        messageTwo.style.color = "blue";
+        messageOne.textContent = "Location: " + data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
+});
